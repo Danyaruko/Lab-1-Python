e9fa22191e2b1a8b36569cad5eb6cce6620c558f
Danyaruko <danilpatana1905@gmail.com>
2021-03-03 18:46:10 +0200

    Improved class and main files readability

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import rally
 
 
 print("Hello, World!\n")
-Car1 = rally.Rally("Ukraine", 1.5, 2, "Stepan Bandera's birthday", "Green", 4)
-print(Car1)
-Car2 = rally.Rally()
-print(Car2)
-Car3 = rally.Rally("Poland", 3, 2, "Independence Day")
-print(Car3)
+shevchenko_night = rally.Rally("Ukraine", 1.5, 2, "Stepan Bandera's birthday", "Green", 4)
+
+print(shevchenko_night)
+independance_day = rally.Rally()
+
+print(independance_day)
+wild_ride = rally.Rally("Poland", 3, 2, "Independence Day")
+
+print(wild_ride)
+mylist = [shevchenko_night, independance_day, wild_ride]
+
+print(mylist)
